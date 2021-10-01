9969f330e0f7012d3bc32d8997d245e1bfb9a030
Subham Karmakar <riju29071999@iitkgp.ac.in>
2021-10-01 22:01:41 +0530

    Fixed constrast issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useEffect} from 'react';
 import Container from './components/Container'
+import { keepTheme } from './utils/theme';
 
-function App() {
-  return (
-    <Container />
-  );
+function App()
+{
+    useEffect(() =>
+    {
+        keepTheme()
+    });
+
+    return (
+        <Container />
+    );
 }
 
 export default App;
